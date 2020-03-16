3f968992375774f97bc7a4c221b7aadfcfcb6b7e
mickgavin <michaelgavin10@gmail.com>
2020-03-16 01:21:49 +0000

    heroku update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{Enactus_e-code}}'));
+app.use(express.static('./dist/Enactus_e-code'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{Enactus_e-code}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/Enactus_e-code/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
